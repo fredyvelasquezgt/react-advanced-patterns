91615b025a7bafc18dcb06ed966af123e7de2fb1
Fredy Velásquez <macacoloco696@gmail.com>
2024-05-23 23:16:33 -0600

    react advanced patterns

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,37 +4,25 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
   
-  React.Children.map(props.children, child => {
-    return React.cloneElement(child)
-  }
-  // 📜 https://react.dev/reference/react/Children
-  // 📜 https://react.dev/reference/react/cloneElement
-  return <Switch on={on} onClick={toggle} />
+ return  React.Children.map(children, child => {
+    console.log(child)
+    return null
+ })
 }
+ 
 
 // 🐨 Flesh out each of these components
 
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = (on, children) => {
-  if(on) {
-    return children
-  }
-}
+const ToggleOn = ({on, children}) => on ? children : null
 
-const ToggleOff = (on, children) => {
-  if(!on) {
-    return children
-  }
-}
+const ToggleOff = (on, children) => on ? null : children
 
-const ToggleButton = (on, toggle) => {
-  return <Switch on={true} toggle={true} />
-}
+const ToggleButton = ({on, toggle}) =>  <Switch on={on} onClick={toggle} />
 
 function App() {
   return (
